e06db6d2336b4e1bd57438362c775d95b6bc30b1
David Estillore <dcestillore@gmail.com>
2020-09-24 20:55:37 +0800

    working countdown!

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$("#getting-started")
+.countdown("2020/12/01", function(event) {
+    $("#day").text(
+        event.strftime('%D')
+    );$("#hours").text(
+        event.strftime('%H')
+    );$("#minutes").text(
+        event.strftime('%M')
+    );$("#seconds").text(
+        event.strftime('%S')
+    );
+});
